1fb27e95ffad2518941aafdecb3d22d6ae37e0aa
Whitman Johnson <whit.ross.j@gmail.com>
2018-03-07 21:45:39 -0500

    refactor(Home): Home is now a functional component

--- Home.react.js
+++ Home.react.js
@@ -3,17 +3,17 @@ import { Text, View, Button } from 'react-native'
 import styles from './Styles.react'
 
 
-export default class HomeScreen extends React.Component {
-    render() {
-        const { navigate } = this.props.navigation
-        return (
-            <View style={styles.container}>
-                <Text>Home Screen</Text>
-                <Button
-                    title="Go to NewRecipe"
+const HomeScreen = props => {
+    const { navigate } = props.navigation
+    return (
+        <View style={styles.container}>
+            <Text>Home Screen</Text>
+            <Button
+                title="Go to NewRecipe"
                 onPress={() => navigate('NewRecipe')}
-                />
-            </View>
-        )
-    }
+            />
+        </View>
+    )
 }
+
+export default HomeScreen
