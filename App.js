aabd46b0b2efb8499c7ab3831d0c93d46758fc21
Whitman Johnson <whit.ross.j@gmail.com>
2018-03-07 20:16:25 -0500

    got a test notification working

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { Notifications as Notify } from 'expo'
 
 export default class App extends React.Component {
   render() {
+      const note = {
+          title: 'Test - Title',
+          body: "this is a test schedule-notification, isn't that nice?"
+      }
+      let date = new Date()
+      let minutes = date.getMinutes()
+      date.setMinutes(minutes + 2)
+      Notify.scheduleLocalNotificationAsync(note, { time: date })
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
